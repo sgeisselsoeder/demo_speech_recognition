6ad81f95fc7dd7f916fc1489dc8a1b3329ae8b36
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2022-02-22 22:43:24 +0100

    adds text to speech again

--- chatbot.py
+++ chatbot.py
@@ -1,38 +1,50 @@
 import speech_recognition as sr
-# import pyttsx3
+import pyttsx3
 import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
 
 listener = sr.Recognizer()
-# engine = pyttsx3.init()
-# voices = engine.getProperty('voices')
-# engine.setProperty('voice', voices[1].id)
+engine = pyttsx3.init()
+engine.setProperty('voice', 'english+f4')
+engine.setProperty('rate', 100)
 
 
 def talk(text):
     print(text)
-    # engine.say(text)
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
 
 
 def get_command():
-    try:
-        with sr.Microphone() as source:
-            print("I am listening ... what you say now will be sent to Google by default!")
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            print(command)
-            searchstring = "computer"
-            if searchstring in command:
-                # print(searchstring + " was mentioned! Removing it.")
-                command = command.replace(searchstring, "")
-            else:
-                command = None
-    except Exception as e:
-        print(e)
-        return None
+    command = None
+    talk("I am listening ... what you say now will be sent to Google by default!")
+    while command is None:
+        try:
+            with sr.Microphone() as source:
+                voice = listener.listen(source)
+                # raises for silence
+                command = listener.recognize_google(voice)
+                command = command.lower()
+                print(command)
+                searchstring = "computer"
+                if searchstring in command:
+                    # print(searchstring + " was mentioned! Removing it.")
+                    command = command.replace(searchstring, "")
+                else:
+                    print("didn't say the magic word")
+                    command = None
+        except sr.UnknownValueError:
+            # Only analyzed silence
+            # print("No recognizable words")
+            pass
+        except Exception as e:
+            print("ERROR")
+            print(e)
+            return None
+
     return command
 
 
@@ -60,7 +72,8 @@ def run():
         talk("I might be alive one day ...")
 
     elif 'joke' in command:
-        talk(pyjokes.get_joke())
+        text = pyjokes.get_joke()
+        talk(text)
 
     elif 'shut down' in command:
         talk("It was a pleasure talking to you")
